117d4997b5f855f5673c942dcfcccb24fd7cbe64
CarrBeat <carrbeat@gmail.com>
2021-11-07 22:39:43 +0300

    added full log code

--- converter/Main.java
+++ converter/Main.java
@@ -5,13 +5,7 @@ import java.util.logging.Logger;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Logger log = Logger.getLogger(Main.class.getName());
-        try {
-            LogManager.getLogManager().readConfiguration(
-                    Main.class.getResourceAsStream("/logging.properties"));
-        } catch (IOException e) {
-            log.info("Don't setup logger config: " + e.toString());
-        }
+        private static final Logger log = Logger.getLogger(String.valueOf(convert.class));
         log.info("программа запущена");
         Scanner input = new Scanner(System.in);
         log.info("выбор перевода");
@@ -22,19 +16,29 @@ public class Main {
         switch (request) {
             case "+":
                 log.info("запрос миль для перевода");
+                try {
                 System.out.println("Введите количество миль для перевода: ");
                 mile = input.nextDouble();
                 km = mile * 1.609;
                 System.out.printf("%f миль равно %f км ", mile, km);
                 log.info("Вывод результата");
+                } catch (Exception ex) {
+                    System.out.println("Вы ввели неправильные данные");
+                    log.info("ERROR");
+                }
                 break;
             case "-":
                 log.info("запрос км для перевода");
+                try {
                 System.out.println("Введите количество км для перевода: ");
                 km = input.nextDouble();
                 mile = km / 1.609;
                 System.out.printf("%f км равно %f миль ", km, mile);
                 log.info("Вывод результата");
+                   } catch (Exception ex) {
+                System.out.println("Вы ввели неправильные данные");
+                log.info("ERROR");
+                    }
                 break;
         }
             log.info("Выполнение программы завершено");
